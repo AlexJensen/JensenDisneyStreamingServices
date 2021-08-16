2322335a03950cd2936eeca34dbcec67f977c539
Alexander Jensen <alexander.jensen@boeing.com>
2021-08-16 14:36:22 -0400

    Moved install all batch file to root

---
+++ installAll.bat
@@ -0,0 +1,7 @@
+cd vcpkg
+vcpkg install curl:x64-windows
+vcpkg install jsoncpp:x64-windows
+vcpkg install gl3w:x64-windows
+vcpkg install glm:x64-windows
+vcpkg install freetype:x64-windows
+vcpkg install boost-filesystem:x64-windows
